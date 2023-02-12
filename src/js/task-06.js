559e540c53ae661d3672b2f985ffb543eece85d9
Artem <artembariga5@gmail.com>
2023-02-12 16:46:50 +0200

    done all tasks

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,11 +1,15 @@
 const inputText = document.querySelector("#validation-input");
+const inputLength = Number(inputText.dataset.length);
 
 const inputValid = event => {
-    if (event.currentTarget.value.length > 6) {
-        return inputText.id = 'validation-input.invalid';
+    if (event.currentTarget.value.length === inputLength) {
+        inputText.classList.remove('invalid');
+        inputText.classList.add('valid');
+    } else {
+        inputText.classList.remove('valid');
+        inputText.classList.add('invalid');
     }
-
-    return inputText.id = 'validation-input.valid';
+    
 }
 
 inputText.addEventListener("input", inputValid);
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -2,9 +2,44 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const inputValue = document.querySelector("[type='number']")
+const input = document.querySelector("[type='number']");
 const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
-console.log(inputValue);
-console.log(btnCreate);
-console.log(btnDestroy);
+const boxesEl = document.getElementById('boxes');
+
+console.log(input.max);
+
+const createBoxes = () =>{
+  let size = 20;
+  const boxArr =[];
+  const error = `<div style ='color: red'>Enter a number from ${input.min} to ${input.max}</div>`;
+
+  if (Number(input.value) >= Number(input.min) && Number(input.value) <= Number(input.max)) {
+    for(let i = 0; i < input.value; i += 1) {
+      size += 10;
+      boxArr.push(`<div class ='elem' style='height: ${size}px; background-color: ${getRandomHexColor()}; width: ${size}px'></div>`);
+    }
+  
+    boxesEl.insertAdjacentHTML("beforebegin", boxArr.join(''));
+  }else {
+    boxesEl.insertAdjacentHTML("beforebegin", error);
+  }
+}
+
+const destroyBoxes = () => {
+  location.reload();
+}
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+
+
+
+
+
+
+
+
+
+
