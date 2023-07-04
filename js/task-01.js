252b7084c2d9cffd01af8778b7ec74cc1719fd93
Artem <artembariga5@gmail.com>
2023-07-04 21:36:04 +0300

    up

--- js/task-01.js
+++ js/task-01.js
@@ -5,4 +5,4 @@ console.log(`Number of categories: ${categories.length}\n\n`);
 categories.forEach((el) => {
     console.log(`Number of categories: ${el.firstElementChild.textContent}`);
     console.log(`Number of categories: ${el.lastElementChild.children.length}\n\n`);
-});
+}); 
